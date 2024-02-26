f63a4575016bc68eb68579ccaef0fbd6897a4e70
BernardoCosmi <bernardo.daddo90@gmail.com>
2024-02-26 14:30:23 +0100

    feat: array print on page

--- index.php
+++ index.php
@@ -2,35 +2,35 @@
     $hotels = [
         [
             'name' => 'Hotel Belvedere',
-            'description' => 'Hotel Belvedere Descrizione',
+            'description' => 'Una vista mozzafiato sul mare.',
             'parking' => true,
             'vote' => 4,
             'distance_to_center' => 10.4
         ],
         [
             'name' => 'Hotel Futuro',
-            'description' => 'Hotel Futuro Descrizione',
+            'description' => 'Confort e modernità al tuo servizio.',
             'parking' => true,
             'vote' => 2,
             'distance_to_center' => 2
         ],
         [
             'name' => 'Hotel Rivamare',
-            'description' => 'Hotel Rivamare Descrizione',
+            'description' => 'La vacanza perfetta a due passi dalla spiaggia.',
             'parking' => false,
             'vote' => 1,
             'distance_to_center' => 1
         ],
         [
             'name' => 'Hotel Bellavista',
-            'description' => 'Hotel Bellavista Descrizione',
+            'description' => "Un'oasi di relax e tranquillità.",
             'parking' => false,
             'vote' => 5,
             'distance_to_center' => 5.5
         ],
         [
             'name' => 'Hotel Milano',
-            'description' => 'Hotel Milano Descrizione',
+            'description' => 'Comodità e praticità nel cuore della città.',
             'parking' => true,
             'vote' => 2,
             'distance_to_center' => 50
@@ -47,6 +47,20 @@
     <title>Document</title>
 </head>
 <body>
-    
+    <div>
+        <ul>
+            <? foreach ($hotels as $hotel): ?>
+            
+                <li>
+                    <strong>Nome: </strong><?php echo $hotel['name']; ?><br>
+                    <strong>Descrizione: </strong><?php echo $hotel['description']; ?><br>
+                    <strong>Parcheggio: </strong><?php echo $hotel['parking'] ? 'Disponibile' : 'Non disponibile'; ?><br>
+                    <strong>Voto: </strong><?php echo $hotel['vote']; ?><br>
+                    <strong>Distanza dal centro: </strong><?php echo $hotel['distance_to_center']; ?> km<br>
+                </li>
+
+            <? endforeach; ?>
+        </ul>
+    </div>
 </body>
 </html>
